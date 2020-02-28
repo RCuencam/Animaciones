9b37526faeb5df5dd73e09c9cf088b54c8cb22b9
Rodrigo <rodrigo@example.com>
2020-02-28 11:24:34 -0500

    terminado

---
+++ js/interaction.js
@@ -0,0 +1,7 @@
+const $blanco = document.getElementById("blanco");
+const $arcoiris=document.getElementById("arcoiris");
+
+$blanco.addEventListener("animationend",(event)=>
+{
+    console.log(event);
+})
